33782eaf62d120b3bf451870ea0c29c32278cb9a
cva199925 <65682441+cva199925@users.noreply.github.com>
2020-06-08 05:50:24 -0500

    Add files via upload

---
+++ out-tsc/src/app/banner/banner.component.js
@@ -0,0 +1,15 @@
+import { __decorate } from "tslib";
+import { Component } from '@angular/core';
+let BannerComponent = class BannerComponent {
+    constructor() {
+        this.title = 'Renueve';
+    }
+};
+BannerComponent = __decorate([
+    Component({
+        selector: 'banner',
+        templateUrl: './app.banner.component.html'
+    })
+], BannerComponent);
+export { BannerComponent };
+//# sourceMappingURL=banner.component.js.map
